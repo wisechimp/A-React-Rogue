e3333a1f915b5e66ab4dc0bb459ecf83ac245aef
shicks1080 <stuart.hicks@gmail.com>
2020-02-14 15:26:48 +0100

    Close of play 14/02/2020
    Flaw with the input manager.

--- src/components/react-rogue.js
+++ src/components/react-rogue.js
@@ -1,14 +1,34 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import InputManager from '../input-manager'
 
 export default ({ width, height, tilesize }) => {
   const canvasRef = useRef()
+  const [player, setPlayer] = useState({ x: 64, y: 128 })
+  let inputManager = new InputManager()
+  const handleInput = (action, data) => {
+    console.log(`handle input: ${action}:${JSON.stringify(data)}`)
+    let newPlayer = { ...player }
+    newPlayer.x += data.x * tilesize
+    newPlayer.y += data.y * tilesize
+    setPlayer(newPlayer)
+  }
 
   useEffect(() => {
-    console.log("Draw canvas")
+    console.log('Bind input')
+    inputManager.bindKeys()
+    inputManager.subscribe(handleInput)
+    return () => {
+      inputManager.unbindKeys()
+      inputManager.unsubscribe(handleInput)
+    }
+  })
+
+  useEffect(() => {
+    console.log("Draw to canvas")
     const canvasContext = canvasRef.current.getContext('2d')
     canvasContext.clearRect(0, 0, width * tilesize, height * tilesize)
     canvasContext.fillStyle='#000'
-    canvasContext.fillRect(12, 22, 16, 16)
+    canvasContext.fillRect(player.x, player.y, 16, 16)
   })
 
   return (
