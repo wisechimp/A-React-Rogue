ae1a818a9015b6a3562d69d2628117cf16bc81f5
shicks1080 <stuart.hicks@gmail.com>
2020-02-24 10:39:29 +0100

    Revert "Tidying the src folder"
    This reverts commit 95b7e2156a53a10c7ff1af59b3dfc1f87f94dfe7.

---
+++ src/input-manager.js
@@ -0,0 +1,45 @@
+class InputManager {
+  observers = []
+
+  subscribe(fn) {
+    this.observers.push(fn)
+  }
+
+  unsubscribe(fn) {
+    this.observers = this.observers.filter(subscriber => subscriber !== fn)
+  }
+
+  broadcast(action, data) {
+    this.observers.forEach(subscriber => subscriber(action, data))
+  }
+
+  handleKeys = event => {
+    event.preventDefault()
+    switch (event.keyCode) {
+      case 37:
+        this.broadcast('move', { x: -1, y: 0 })
+        break
+      case 38:
+        this.broadcast('move', { x: 0, y: -1 })
+        break
+      case 39:
+        this.broadcast('move', { x: 1, y: 0 })
+        break
+      case 40:
+        this.broadcast('move', { x: 0, y: 1 })
+        break
+      default:
+        break
+    }
+  }
+
+  bindKeys() {
+    document.addEventListener('keydown', this.handleKeys)
+  }
+
+  unbindKeys() {
+    document.removeEventListener('keydown', this.handleKeys)
+  }
+}
+
+export default InputManager
